26854370e10fdaf024237030452468ba227fe395
Pratikuser <ps0566179@gmail.com>
2026-06-26 20:48:40 +0530

    Add files via upload
    footer.php (Minor layout adjustments)

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <div class="footer-container">
+            <p>&copy; <?php echo date('Y'); ?> <?php echo htmlspecialchars($my_name ?? 'Pratik Singh'); ?> - Portfolio Task 1</p>
+        </div>
+    </footer>
+</body>
+</html>
